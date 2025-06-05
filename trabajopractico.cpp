ab7641174320a0afac98403ef7bb1feb36af0c86
DavidPalaciosCv <davidip0880@gmail.com>
2025-06-05 15:34:02 -0300

    Actualizar trabajopractico.cpp
    correcciones

--- trabajopractico.cpp
+++ trabajopractico.cpp
@@ -198,156 +198,156 @@ int main(){
 	return 0;
 }
 	
-	void duelo(pj* &a,pj* &b, char c, char d, char e){
-		cout<<a->getnom()<<" inicia el combate"<<endl<<endl;
+void duelo(pj* &a,pj* &b, char c, char d, char e){
+	cout<<a->getnom()<<" inicia el combate"<<endl<<endl;
+	do
+	{
 		do
 		{
-			do
+			cout<<"turno de "<<a->getnom()<<endl;
+			cout<<"1 - Golpe"<<endl;
+			if(c=='a' || c=='A'){
+				cout<<"2 - Lanzarrocas"<<endl;
+			}
+			if(c=='b' || c=='B'){
+				cout<<"2 - Lanzallamas"<<endl;
+			}
+			if(c=='c' || c=='C'){
+				cout<<"2 - Stunami"<<endl;
+			}
+			if(c=='d' || c=='D'){
+				cout<<"2 - Tornado"<<endl;
+			}
+			cout<<"3 - Bloquear"<<endl;
+			cout<<"4 - Curarse"<<endl<<endl;
+			mostrarsalud(a->gethp(),c);
+			cout<<"DEFENSA: "<<a->getesc()<<endl;
+			cin>>e;
+			switch (e)
 			{
-				cout<<"turno de "<<a->getnom()<<endl;
-				cout<<"1 - Golpe"<<endl;
+			case '1':
+				system("cls");
+				a->golpear(b);
+				break;
+			case '2':
+				system("cls");
 				if(c=='a' || c=='A'){
-					cout<<"2 - Lanzarrocas"<<endl;
+					a->lanzarroca(b,a->getdano());
 				}
 				if(c=='b' || c=='B'){
-					cout<<"2 - Lanzallamas"<<endl;
+					a->lanzallamas(b,a->getdano());
 				}
 				if(c=='c' || c=='C'){
-					cout<<"2 - Stunami"<<endl;
+					a->sunami(b,a->getdano());
 				}
 				if(c=='d' || c=='D'){
-					cout<<"2 - Tornado"<<endl;
+					a->tornado(b,a->getdano());
 				}
-				cout<<"3 - Bloquear"<<endl;
-				cout<<"4 - Curarse"<<endl<<endl;
-				mostrarsalud(a->gethp(),c);
-				cout<<"DEFENSA: "<<a->getesc()<<endl;
-				cin>>e;
-				switch (e)
-				{
-				case '1':
-					system("cls");
-					a->golpear(b);
-					break;
-				case '2':
-					system("cls");
-					if(c=='a' || c=='A'){
-						a->lanzarroca(b,a->getdano());
-					}
-					if(c=='b' || c=='B'){
-						a->lanzallamas(b,a->getdano());
-					}
-					if(c=='c' || c=='C'){
-						a->sunami(b,a->getdano());
-					}
-					if(c=='d' || c=='D'){
-						a->tornado(b,a->getdano());
-					}
-					break;
-				case '3':
-					system("cls");
-					a->bloquear();
-					break;
-				case '4':
-					system("cls");
-					if((tolower(c) != 'c' && a->gethp() == 100) || (tolower(c) == 'c' && a->gethp() == 150)){
-						cout<<"Salud al maximo"<<endl;
-						e='6';
-						break;
-					}
-					if(c=='c' || c=='C')
-																	a->curaragua(a);
-					else
-						a->curar();
-					break;
-				default:
-					system("cls");
-					cout<<"  No es opcion  >:(  "<<endl;
+				break;
+			case '3':
+				system("cls");
+				a->bloquear();
+				break;
+			case '4':
+				system("cls");
+				if((tolower(c) != 'c' && a->gethp() == 100) || (tolower(c) == 'c' && a->gethp() == 150)){
+					cout<<"Salud al maximo"<<endl;
+					e='6';
 					break;
 				}
-			} while (e!='1' && e!='2' && e!='3' && e!='4');
-			if(b->gethp()<=0)break;
-			do
+				if(c=='c' || c=='C')
+					a->curaragua(a);
+				else
+					a->curar();
+				break;
+			default:
+				system("cls");
+				cout<<"  No es opcion  >:(  "<<endl;
+				break;
+			}
+		} while (e!='1' && e!='2' && e!='3' && e!='4');
+		if(b->gethp()<=0)break;
+		do
+		{
+			cout<<"Turno de "<<b->getnom()<<endl;
+			cout<<"1 - Golpe"<<endl;
+			if(d=='a' || d=='A'){
+				cout<<"2 - Lanzarrocas"<<endl;
+			}
+			if(d=='b' || d=='B'){
+				cout<<"2 - Lanzallamas"<<endl;
+			}
+			if(d=='c' || d=='C'){
+				cout<<"2 - Stunami"<<endl;
+			}
+			if(d=='d' || d=='D'){
+				cout<<"2 - Tornado"<<endl;
+			}
+			cout<<"3 - Bloquear"<<endl;
+			cout<<"4 - Curarse"<<endl<<endl;
+			mostrarsalud(b->gethp(),d);
+			cout<<"DEFENSA: "<<b->getesc()<<endl;
+			cin>>e;
+			switch (e)
 			{
-				cout<<"Turno de "<<b->getnom()<<endl;
-				cout<<"1 - Golpe"<<endl;
+			case '1':
+				system("cls");
+				b->golpear(a);
+				break;
+			case '2':
+				system("cls");
 				if(d=='a' || d=='A'){
-					cout<<"2 - Lanzarrocas"<<endl;
+					b->lanzarroca(a,b->getdano());
 				}
 				if(d=='b' || d=='B'){
-					cout<<"2 - Lanzallamas"<<endl;
+					b->lanzallamas(a,b->getdano());
 				}
 				if(d=='c' || d=='C'){
-					cout<<"2 - Stunami"<<endl;
+					b->sunami(a,b->getdano());
 				}
 				if(d=='d' || d=='D'){
-					cout<<"2 - Tornado"<<endl;
+					b->tornado(a,b->getdano());
 				}
-				cout<<"3 - Bloquear"<<endl;
-				cout<<"4 - Curarse"<<endl<<endl;
-				mostrarsalud(b->gethp(),d);
-				cout<<"DEFENSA: "<<b->getesc()<<endl;
-				cin>>e;
-				switch (e)
-				{
-				case '1':
-					system("cls");
-					b->golpear(a);
-					break;
-				case '2':
-					system("cls");
-					if(d=='a' || d=='A'){
-						b->lanzarroca(a,b->getdano());
-					}
-					if(d=='b' || d=='B'){
-						b->lanzallamas(a,b->getdano());
-					}
-					if(d=='c' || d=='C'){
-						b->sunami(a,b->getdano());
-					}
-					if(d=='d' || d=='D'){
-						b->tornado(a,b->getdano());
-					}
-					break;
-				case '3':
-					system("cls");
-					b->bloquear();
-					break;
-				case '4':
-					system("cls");
-					if((tolower(d) != 'c' && b->gethp() == 100) || (tolower(d) == 'c' && b->gethp() == 150)){
-						cout<<"Salud al maximo"<<endl;
-						e='6';
-						break;
-					}
-					if(d=='c' || d=='C')
-																	b->curaragua(b);
-					else
-						b->curar();
-					break;
-				default:
-					system("cls");
-					cout<<"No es opcion"<<endl;
+				break;
+			case '3':
+				system("cls");
+				b->bloquear();
+				break;
+			case '4':
+				system("cls");
+				if((tolower(d) != 'c' && b->gethp() == 100) || (tolower(d) == 'c' && b->gethp() == 150)){
+					cout<<"Salud al maximo"<<endl;
+					e='6';
 					break;
 				}
-			} while (e!='1' && e!='2' && e!='3' && e!='4');
-			if(a->gethp()<=0)break;
-		} while (a->gethp()>0 && b->gethp()>0);
-	}
+				if(d=='c' || d=='C')
+					b->curaragua(b);
+				else
+					b->curar();
+				break;
+			default:
+				system("cls");
+				cout<<"No es opcion"<<endl;
+				break;
+			}
+		} while (e!='1' && e!='2' && e!='3' && e!='4');
+	    if(a->gethp()<=0)break;
+	} while (a->gethp()>0 && b->gethp()>0);
+}
 		
-	void mostrarsalud(int a, char b){
-		int hpMax, barras=10;
-		if(b=='c' || b=='C')
-			hpMax=150; 
-		else
-			hpMax=100; 
-		int bloques = (a*barras)/hpMax;
-		cout << "[";
-		for (int i = 0; i < bloques; i++) {
-			cout << "#";
-		}
-		for (int i=bloques;i<barras;i++) {
-			cout<<"-";
-		}
-		cout<<"] "<<a<<"/"<<hpMax<<" HP"<<endl;
+void mostrarsalud(int a, char b){
+	int hpMax, barras=10;
+	if(b=='c' || b=='C')
+		hpMax=150; 
+	else
+		hpMax=100; 
+	int bloques = (a*barras)/hpMax;
+	cout << "[";
+	for (int i = 0; i < bloques; i++) {
+		cout << "#";
 	}
+	for (int i=bloques;i<barras;i++) {
+		cout<<"-";
+	}
+	cout<<"] "<<a<<"/"<<hpMax<<" HP"<<endl;
+}
